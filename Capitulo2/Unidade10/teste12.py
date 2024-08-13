7ca9602731b1d045f84408043b4815a72ba1d7fe
Jean Macedo <jeancmacedo1@gmail.com>
2024-08-13 18:46:56 -0300

    Commit
    New commit

---
+++ Capitulo2/Unidade10/teste12.py
@@ -0,0 +1,9 @@
+a_list = [10, 20, 30, 40, 50, 60, 70, 80]
+print(a_list,"1\n")
+print(a_list[1:5],"2\n")
+print(a_list[0:5],"3\n")
+print(a_list[1:],"4\n")
+print(a_list[:5],"5\n")
+print(a_list[:],"6\n")
+print(a_list[-8:],"7 - negativos\n")
+print(a_list[-8:-1],"8 - negativos\n")
---
+++ Capitulo2/Unidade10/teste13.py
@@ -0,0 +1,3 @@
+n_list = [11, 22, 33, 44, 55, 66]
+print (88 in n_list) 
+print (55 in n_list) 
