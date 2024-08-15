b799c7c9b44e37fa3a3e8b3dcca3128e7f274f94
Jean Macedo <jeancmacedo1@gmail.com>
2024-08-15 19:54:01 -0300

    commit
    new commit

--- Capitulo2/Unidade12/2.py
+++ Capitulo2/Unidade12/2.py
@@ -10,3 +10,22 @@ str1 = 'hello'
 str2 = ' world' 
 print(str1 + str2,'\n')
 
+print(list(range (10)) + list(range(10,20)),'\n')
+
+print(tuple(range(10)) + tuple(range(10, 20)),'\n')
+
+list1 = [11, 22, 33, 44] * 2
+print(list1,'\n')
+
+tup1 = (1, 2, 3)
+print(tup1 * 2,'\n')
+
+str2 = 'hello'
+print(str2 * 3,'\n')
+
+ran = tuple(range(5)) * 3
+print(ran,'\n')
+
+list1 = [11, 11, 11, 22, 33, 44]
+print(list1.count(11),'\n') #Números de elementos específicos
+
